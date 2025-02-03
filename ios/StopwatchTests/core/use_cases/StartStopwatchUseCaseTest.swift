c92d64bd018411c747323523817023f7df399782
Rafhael Payao <rafaeltmbr@gmail.com>
2025-02-02 22:19:05 -0300

    test: improve iOS unit tests

--- ios/StopwatchTests/core/use_cases/StartStopwatchUseCaseTest.swift
+++ ios/StopwatchTests/core/use_cases/StartStopwatchUseCaseTest.swift
@@ -22,4 +22,23 @@ struct StartStopwatchUseCaseTest {
         #expect(store.state.status == .running)
         #expect(service.state.isRunning)
     }
+    
+    @Test func start_shouldResumeStopwathAfterPause() async throws {
+        let store = MutableStateStoreImpl(StopwatchState(), EventEmitterImpl())
+        let service = TimerServiceImpl(EventEmitterImpl())
+        let startUseCase = StartStopwatchUseCaseImpl(store, service)
+        let pauseUseCase = PauseStopwatchUseCaseImpl(store, service)
+
+        await startUseCase.execute()
+        #expect(store.state.status == .running)
+        #expect(service.state.isRunning)
+        
+        await pauseUseCase.execute()
+        #expect(store.state.status == .paused)
+        #expect(!service.state.isRunning)
+        
+        await startUseCase.execute()
+        #expect(store.state.status == .running)
+        #expect(service.state.isRunning)
+    }
 }
